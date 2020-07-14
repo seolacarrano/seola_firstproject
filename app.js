b41b0bbdc1d9fd676d3beb209778c1a59b5f81b3
seolacarrano <seola6@gmail.com>
2020-07-14 14:16:20 -0400

    commit

--- app.js
+++ app.js
@@ -33,7 +33,28 @@ fetch(url)
    })
    }
 
-   
+   /*
+   //hamburger
+   $( document ).ready(function() {
+       
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+    });*/
+
+       
    //hamburger
    $( document ).ready(function() {
        
