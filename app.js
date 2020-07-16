07d8a9ec638c25c5c12520592c7ac8a7607131bb
seolacarrano <seola6@gmail.com>
2020-07-16 14:49:49 -0400

    trying to open a link in a new tab

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ fetch(url)
         $div.append($('<h2>').addClass("nameofproject").text(project.title))
         $div.append($('<img>').addClass("imageofproject").attr('src', project.image))
         $div.append($('<p>').addClass("description").text(project.description))
-        $div.append($('<a>').addClass("linktoproject").attr('href', project.url).text('LINK >>>').html('<i class="fab fa-codepen fa-2x"></i>'))
+        $div.append($('<a>').attr('href', project.url).html('<i class="fab fa-codepen fa-2x"></i>'))
+        //$div.append($('<a>').attr('href', project.url).html('<i class="fab fa-codepen fa-2x"></i>'))
         //$div.append($('<a>').addClass("linktoproject").attr('href', project.url).text('LINK >>>'))
         return $div
     }
